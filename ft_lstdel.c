983833898a7f49fb13bdf1f53637b6668bf7bf75
Kodoque1 <zakaria.addi@gmail.com>
2025-11-04 23:07:21 +0100

    update libft

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -1,10 +1,10 @@
 #include "libft.h"
 
-void ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+void ft_lstclear(t_list **alst, void (*del)(void *, size_t))
 {
     if(*alst)
     {
-        ft_lstdel(&((*alst)->next), del);
+        ft_lstclear(&((*alst)->next), del);
         ft_lstdelone(alst, del);
     }   
 }
