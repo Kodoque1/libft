640f29d643ba39a9f5e26a684829d0167f249039
Github <zakaria.addi@gmail.com>
2025-11-06 09:54:09 +0100

    add ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+int strlcpy(char *dst, const char *src, size_t size)
+{
+    size_t i;
+    i = 0;
+    while(src[i < size)
+    {
+        dest[len + i] = '\0';
+        i++;
+    }
+    return (i); 
+}
